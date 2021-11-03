049977acf70412233ae1c56acd27ba216fe591ba
yougyoung <yougyoung@pubg.com>
2021-11-03 21:53:02 +0900

    #0. Move application code to app

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ from flask_mail import Mail, Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# app = Flask(__name__)
+app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'hard to guess string'
 # app.config['SQLALCHEMY_DATABASE_URI'] =\
 #     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
@@ -33,40 +33,42 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 
 
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
+# << models.py
+# class Role(db.Model):
+#     __tablename__ = 'roles'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64), unique=True)
+#     users = db.relationship('User', backref='role', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return '<Role %r>' % self.name
+#
+#
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(64), unique=True, index=True)
+#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+#
+#     def __repr__(self):
+#         return '<User %r>' % self.username
+# >>
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
-
-def send_email(to, subject, template, **kwargs):
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
-    msg.body = render_template(template + '.txt', **kwargs)
-    msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
-
+# << email.py
+# def send_async_email(app, msg):
+#     with app.app_context():
+#         mail.send(msg)
+#
+#
+# def send_email(to, subject, template, **kwargs):
+#     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+#                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+#     msg.body = render_template(template + '.txt', **kwargs)
+#     msg.html = render_template(template + '.html', **kwargs)
+#     thr = Thread(target=send_async_email, args=[app, msg])
+#     thr.start()
+#     return thr
+# >>
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
