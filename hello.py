8a50061565f76b408f2db3f751e193906ff3d7b4
yougyoung <yougyoung@pubg.com>
2021-12-27 16:18:12 +0900

    Chapter 1

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Your browser is %s</h1>' % user_agent
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello %s!</h1>' % name
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
