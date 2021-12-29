31d52b413a0bf4ca3fee4af558ae176c71cae312
yougyoung <yougyoung@pubg.com>
2021-12-29 14:51:45 +0900

    Chapter 4

--- hello.py
+++ hello.py
@@ -1,12 +1,21 @@
 from datetime import datetime
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -19,12 +28,19 @@ def internval_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # user_agent = request.headers.get('User-Agent')
-    # return '<h1>Your browser is %s</h1>' % user_agent
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('New name?')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
     return render_template('index.html',
-                           current_time=datetime.utcnow())
+                           current_time=datetime.utcnow(),
+                           form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
