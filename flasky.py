d162dfc563cec8e2a6906d79723500c64b6ec9b9
yougyoung <yougyoung@pubg.com>
2022-01-09 15:53:44 +0900

    db init --> db migrate --> db upgrade

--- flasky.py
+++ flasky.py
@@ -1,10 +1,12 @@
 import os
 import click
-from flask_migrate import Migrate
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
 migrate = Migrate(app, db)
 
 
@@ -13,7 +15,12 @@ def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
 
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
 @app.cli.command()
+@manager.command
 @click.argument('test_names', nargs=-1)
 def test(test_names):
     """Run the unit tests."""
@@ -23,3 +30,7 @@ def test(test_names):
     else:
         tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
