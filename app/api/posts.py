bbe409a654d4616ce76da21833f2d1b287cf3d76
yougyoung <yougyoung@pubg.com>
2022-01-23 22:40:37 +0900

    GET

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,6 +1,7 @@
 from flask import request, g, jsonify
 
 from . import api
+from .authentication import auth
 from .. import db
 from ..models import Post
 
@@ -11,4 +12,18 @@ def new_post():
     post.author = g.current_user
     db.session.add(post)
     db.session.commit()
+    return jsonify(post.to_json())
+
+
+@api.route('/posts/')
+@auth.login_required
+def get_posts():
+    posts = Post.query.all()
+    return jsonify({'posts': [post.to_json() for post in posts]})
+
+
+@api.route('/posts/<int:id>')
+@auth.login_required
+def get_post(id):
+    post = Post.query.get_or_404(id)
     return jsonify(post.to_json())
