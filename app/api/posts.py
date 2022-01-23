1d8ea1f4cb633a97befc0b97b3162bf3c3868380
yougyoung <yougyoung@pubg.com>
2022-01-23 22:51:31 +0900

    permission_required

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,18 +1,22 @@
-from flask import request, g, jsonify
+from flask import request, g, jsonify, url_for
 
 from . import api
 from .authentication import auth
+from .decorators import permission_required
+from .errors import forbidden
 from .. import db
-from ..models import Post
+from ..models import Post, Permission
 
 
 @api.route('/posts/', methods=['POST'])
+@permission_required(Permission.WRITE)
 def new_post():
     post = Post.from_json(request.json)
     post.author = g.current_user
     db.session.add(post)
     db.session.commit()
-    return jsonify(post.to_json())
+    return jsonify(post.to_json(), 201,
+                   {'Location': url_for('api.get_post', id=post.id)})
 
 
 @api.route('/posts/')
@@ -22,8 +26,22 @@ def get_posts():
     return jsonify({'posts': [post.to_json() for post in posts]})
 
 
-@api.route('/posts/<int:id>')
+@api.route('/posts/<int:post_id>')
 @auth.login_required
-def get_post(id):
-    post = Post.query.get_or_404(id)
+def get_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return jsonify(post.to_json())
+
+
+@api.route('/posts/<int:post_id>', methods=['PUT'])
+@permission_required(Permission.WRITE)
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if g.current_user != post.author and \
+            not g.current_user.can(Permission.ADMIN):
+        return forbidden('Insufficient permissions')
+
+    post.body = request.json.get('body', post.body)
+    db.session.add(post)
+    db.session.commit()
     return jsonify(post.to_json())
